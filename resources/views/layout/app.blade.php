e213ce9a7e30af9365b3b4f58c56ce0198043990
micaljohn60 <micaljohn60@gmail.com>
2021-05-16 16:32:51 +0630

    Add Custom Style Files

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -4,13 +4,17 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Neptune Technology</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
         <!-- Bootstrap -->
 
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+        
+        <!-- CSS -->
+        <link href="/css/styleb.css" rel="stylesheet">
+        <link href="/css/stylem.css" rel="stylesheet">
 
         <!-- Styles -->
         <style>
