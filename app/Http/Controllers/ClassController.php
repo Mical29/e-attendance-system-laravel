e0cc517d1292262a81c5d6d5014f3c4ec8e371cd
micaljohn60 <micaljohn60@gmail.com>
2021-05-31 11:40:56 +0630

    Saved Program Worked

--- app/Http/Controllers/ClassController.php
+++ app/Http/Controllers/ClassController.php
@@ -14,24 +14,22 @@ class ClassController extends Controller
     }
 
     public function create(){
-        $program = new Program();
-        
         return view('admin/create_class');
     }
 
     public function store(Request $request){
-        request()->validate([
-            'programname' => 'required',
-            'programimg' => 'required',
-            'programfeeslocal' => 'required',
-            'programfeesforeign' => 'required',
-            'programfeesmessage' => 'required',
+        $programs = new Program();
+        $programs->program_name = $request->input('programname');
+        $programs->program_image = $request->input('programimg');
+        $programs->domestic_student = $request->input('programfeeslocal');
+        $programs->international_student = $request->input('programfeesforeign');
+        $programs->program_fees_message = $request->input('programfeesmessage');
+        $programs->eligibility_and_requirements = $request->input('programRequirement');
+      
 
-        ]);
-        $input = $request->all();
-        Program::create($request->all());
+       
+        $programs->save();
         echo'Successfully Saved :)';
-
     }
 
     public function destory(){
--- app/Models/Program.php
+++ app/Models/Program.php
@@ -1,5 +1,5 @@
 <?php
-
+namespace App\Http\Controllers;
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Program extends Model
 {
     use HasFactory;
+    protected $table = 'programs';
+
+        protected $fillable = [
+            'porgram_name',
+            'program_image',
+            'domestic_student',
+            'international_student',
+            'program_fees_message'
+        ];
 }
