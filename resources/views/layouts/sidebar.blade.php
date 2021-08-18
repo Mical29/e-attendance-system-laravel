5357703397177e8798a8b5b81049ff7e8a49dd4d
YeLinnAungGautam <davidgautam.1234@gmail.com>
2021-08-18 20:26:51 +0630

    Side Bar Added

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,3 +1,15 @@
-<div class="bg-dark text-light">
-    <h3>This is menu</h3>
+<div class="text-light main_bg">
+<div class="side-navbar active-nav d-flex justify-content-between flex-wrap flex-column" id="sidebar"> 
+                        <ul class="nav flex-column text-white w-100 mt-3">
+                            <a href="#" style="text-decoration:none;"><li href="#" class="nav-link"><span class="mx-2"><i class="fas fa-home"></i> Home</span></li></a>
+                            <a href="#" style="text-decoration:none;"><li href="#" class="nav-link"><span class="mx-2">Edit Details</span></li></a>
+                            <a href="#" style="text-decoration:none;"><li href="#" class="nav-link"><span class="mx-2">Profile Gallery</span></li></a>
+                            <a href="#" style="text-decoration:none;"><li href="#" class="nav-link"><span class="mx-2">Job Search</span></li></a>
+                            <a href="#" style="text-decoration:none;"><li href="#" class="nav-link"><span class="mx-2">Post A Job</span></li></a>
+                            <a href="#" style="text-decoration:none;"><li href="#" class="nav-link"><span class="mx-2">Manage Posted Job</span></li></a>
+                            <a href="#" style="text-decoration:none;"><li href="#" class="nav-link"><span class="mx-2">Messages</span></li></a>
+                            <a href="create-an-invoice.php" style="text-decoration:none;"><li href="#" class="nav-link"><span class="mx-2">Create an Invoice</span></li></a>
+                            <a href="#" style="text-decoration:none;"><li href="#" class="nav-link"><span class="mx-2">Archived Invoices</span></li></a>
+                        </ul>
+                    </div>
 </div>
